71bb1b71691e7453fc3958aec98294cd54fe2ea8
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2024-11-15 10:38:22 +0100

    Version 0.1

---
+++ JS/homeLogic.js
@@ -0,0 +1,8 @@
+//  https://striveschool-api.herokuapp.com/api/product/
+
+fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MGIzMzhhZDEyOTAwMTU4NzZiYzMiLCJpYXQiOjE3MzE2NjA1OTUsImV4cCI6MTczMjg3MDE5NX0.ccCT6cgYx8NXS5OCNd5ziRxjHXIpQOtrRawHAIDl_I0",
+  },
+}).then;
